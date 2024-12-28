dc7171a18613ea795878385593a4247c3e99fa67
s-karmakar <47075235+s-karmakar@users.noreply.github.com>
2024-12-28 17:06:41 +0530

    app file

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Namaste React</h1>
+      <p>Welcome to my React application!</p>
+    </div>
+  );
+}
+
+export default App;
