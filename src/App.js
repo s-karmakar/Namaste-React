8eb122b5ebf64585c81b504e51024e034c4885f3
s-karmakar <sk.karmakar02@gmail.com>
2025-04-07 02:48:43 +0530

    fixing the directory and file position  and all before tailwind experiment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 // import "../index.css";
-import "../index.css";
+import "./index.css";
 import Header from "./Header";
 import Body from "./Body";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router";
